01509763093ebed84ecb9b7092d349d6e583c702
dasivenkat <144246970+dasivenkat@users.noreply.github.com>
2026-07-28 22:43:05 +0530

    Update ec2instancedemo.tf

--- ec2instancedemo.tf
+++ ec2instancedemo.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region     = "ap-south-2"
 }
 
-resource "aws_instance" "myec2" {
-  ami           = "ami-0b2ac1bf38835e348"
-  instance_type = "t3.micro"
-  tags = {
-    name = "webserver"
-  }
-}
+#resource "aws_instance" "myec2" {
+ # ami           = "ami-0b2ac1bf38835e348"
+  #instance_type = "t3.micro"
+  #tags = {
+   # name = "webserver"
+  #}
+#}
